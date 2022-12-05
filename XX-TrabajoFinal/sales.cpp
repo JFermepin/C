3065be96a71c6fdbc027b01f0414e29ab37c9b0b
Jerof01 <103324150+JFermepin@users.noreply.github.com>
2022-12-05 19:19:09 -0300

    subida de datos al archivo binario

--- XX-TrabajoFinal/sales.cpp
+++ XX-TrabajoFinal/sales.cpp
@@ -21,15 +21,42 @@ using Data = array<array<array<int,12>,3>,4>;	//REGION - VENDEDOR - MES
 ***	PROTOTIPOS ***
 ******************/
 
+Data LeerDatosTXT ();
+void CargarDatosBIN (Data& data);
+
+
 /***********
 ***	MAIN ***
 ************/
 
 int main()
 {
+    Data data{};
+    CargarDatosBIN (data);
 	return 0;
 }
 
 /******************************
 ***	FUNCIONES - DEFNICIONES ***
 *******************************/
+
+Data LeerDatosTXT (){
+
+    Data data{};
+    for(int region{}, vendedor{}, mes{}, importe{}; cin >> region >> vendedor >> mes >> importe; )
+        data.at(region-1).at(vendedor-1).at(mes-1) += importe;
+
+    return data;
+}
+
+
+void CargarDatosBIN (Data& data){
+
+    data = LeerDatosTXT();
+
+    ofstream file;
+    file.open("data.bin", ios::out | ios::binary);
+    file.write(reinterpret_cast<char *>(&data), sizeof(data));
+	file.close();
+
+}
