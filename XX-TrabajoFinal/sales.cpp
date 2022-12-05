a21a5ede92f25040af314a4003158208419980df
Jerof01 <103324150+JFermepin@users.noreply.github.com>
2022-12-05 17:55:06 -0300

    prototipo

---
+++ XX-TrabajoFinal/sales.cpp
@@ -0,0 +1,35 @@
+/***************
+***	INCLUDES ***
+****************/
+
+#include <iostream>
+#include <array>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <vector>
+
+/*************
+***	USINGS ***
+**************/
+
+using namespace std;
+
+using Data = array<array<array<int,12>,3>,4>;	//REGION - VENDEDOR - MES
+
+/*****************
+***	PROTOTIPOS ***
+******************/
+
+/***********
+***	MAIN ***
+************/
+
+int main()
+{
+	return 0;
+}
+
+/******************************
+***	FUNCIONES - DEFNICIONES ***
+*******************************/
